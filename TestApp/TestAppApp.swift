f1f5cae512627eced3bdf2a73c708cd56f3441b6
Nikhil Sehgal <nikhil.sehgal@vastmindz.com>
2024-12-02 12:44:23 +1100

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by admin on 02/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
